34eb9d49a43ce78ba3c788237ab023bc090688f6
Vitor Correa <110726738+VitorCorrea98@users.noreply.github.com>
2022-09-08 10:32:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,88 +1,101 @@
 //  Quantidade de produtos das 2 caixas acima  //
 
-var sumEl1 = document.getElementById("sum-el1");
+const INCREASE1 = document.getElementById("increase1")
+const SUMEL1 = document.getElementById("sum-el1");
+const DECREASE1 = document.getElementById("decrease1")
 
-var count1 = 0;
+let totalEl = document.getElementById("total-el")
 
+let count1 = 0;
 
+let valor1 = 54.99 * count1
 
-var totalEl = document.getElementById("total-el")
-
-var valor1 = 73.99 * count1
-
-var valor2 = 93.99 * count2
-
-
+totalEl.innerText += 19
 
 
-
-function increase1(){
+INCREASE1.addEventListener("click", function increase1(){
 
     count1 += 1;
-    sumEl1.innerHTML = count1;
+    SUMEL1.innerHTML = count1;
 
-    var valor1 = 73.99 * count1
+    let valor1 = 54.99 * count1
 
-    var valor2 = 93.99 * count2
+    let valor2 = 74.99 * count2
 
     let total = valor1 + valor2
+
+    let totalGeral = total + 19
     
-    totalEl.innerHTML ="$" + total
+    totalEl.innerHTML ="$" + totalGeral.toFixed(2)
 
-}
+})
 
-function decrease1(){
+DECREASE1.addEventListener("click",function decrease1(){
 
     count1 -= 1;
-    sumEl1.innerHTML = count1;
+    SUMEL1.innerHTML = count1;
 
-    var valor1 = 73.99 * count1
+    let valor1 = 54.99 * count1
 
-    var valor2 = 93.99 * count2
+    let valor2 = 74.99 * count2
 
     let total = valor1 + valor2
+
+    let totalGeral = total + 19
     
-    totalEl.innerHTML ="$" + total
+    totalEl.innerHTML ="$" + totalGeral.toFixed(2)
 
-}
-
-
+})
 
 
-var sumEl2 = document.getElementById("sum-el2");
 
-var count2 = 0;
 
-function increase2(){
+const INCREASE2 = document.getElementById("increase2")
+const SUMEL2 = document.getElementById("sum-el2");
+const DECREASE2 = document.getElementById("decrease2")
+
+let count2 = 0;
+
+let valor2 = 74.99 * count2
+
+
+
+INCREASE2.addEventListener("click",function increase2(){
 
     count2 += 1;
-    sumEl2.innerHTML = count2;
+    SUMEL2.innerHTML = count2;
 
-    var valor1 = 73.99 * count1
+    let valor1 = 54.99 * count1
 
-    var valor2 = 93.99 * count2
+    let valor2 = 74.99 * count2
 
     let total = valor1 + valor2
+
+    let totalGeral = total + 19
     
-    totalEl.innerHTML ="$" + total
+    totalEl.innerHTML ="$" + totalGeral.toFixed(2)
 
-}
+    document.getElementById("increase").disabled = false
 
-function decrease2(){
+})
 
+DECREASE2.addEventListener("click",function decrease2(){
+
+    
     count2 -= 1;
-    sumEl2.innerHTML = count2;
-
-    var valor1 = 73.99 * count1
-
-    var valor2 = 93.99 * count2
-
+    SUMEL2.innerHTML = count2;
+    
+    let valor1 = 54.99 * count1
+    
+    let valor2 = 74.99 * count2
+    
     let total = valor1 + valor2
     
-    totalEl.innerHTML ="$" + total
+    let totalGeral = total + 19
     
-}
-
+    totalEl.innerHTML ="$" + totalGeral.toFixed(2)
+    
+})
 
 
 //  Quantidade de produtos das 2 caixas acima  //
@@ -92,4 +105,4 @@ function decrease2(){
 // Valor total dos produto
 
 
-    totalEl.innerText += 0
+    
